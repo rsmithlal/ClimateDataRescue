746110bfa6882c503070aeb8b9ca5ad564bdb129
Robert Smith <rsmithlal@gmail.com>
2021-02-22 02:49:23 -0330

    text if pid file exists so it doesn't crash app on heroku

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,5 +3,11 @@
 # ./docker/wait-for-services.sh
 ./docker/prepare-db.sh
 # mkdir ./tmp/pids
-rm -f tmp/pids/server.pid
+pid = tmp/pids/server.pid
+
+if test -f "$pid"; then
+  echo "$pid exists."
+  rm -f $pid
+fi
+
 bundle exec puma -C config/puma.rb
