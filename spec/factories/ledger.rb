78efd6f96c97930630d87353c9d560a5738e3026
Robert Smith <rsmithlal@gmail.com>
2021-03-11 23:58:06 -0330

    update ledger factory to have more random value range for the name

--- spec/factories/ledger.rb
+++ spec/factories/ledger.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :ledger do
-    number = Faker::Number.unique.between(from: 1, to: 5000)
+    number = Faker::Number.unique.between(from: 1, to: 7000)
     title { "Ledger #{number}" }
     ledger_type { Faker::Number.between(from: 100, to: 500) }
   end
